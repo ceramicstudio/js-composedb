a4b5a7c17e8bf0a52753cd8a1a30887bcf430751
Artur Wdowiarski <arturwdowiarski@me.com>
2022-08-09 12:11:15 +0100

    Fixed typo

--- packages/cli/globalSetup.js
+++ packages/cli/globalSetup.js
@@ -53,7 +53,7 @@ export default async function globalSetup() {
   //FIXME: Don't hardcode the @ceramicnetwork/cli version. Remove the hard-coded version once we update js-composedb to work with the latest version
   await setup({
     command:
-      `IPFS_PATH=\'${GOIPFS_DIR_PATH}\' CERAMIC_ENABLE_EXPERIMENTAL_INDEXING=\'true\' && pnpm dlx @ceramicnetwork/cli@2.4.0 daemon --config ${CONFIG_PATH}`,
+      `IPFS_PATH=\'${GOIPFS_DIR_PATH}\' CERAMIC_ENABLE_EXPERIMENTAL_INDEXING=\'true\' pnpm dlx @ceramicnetwork/cli@2.4.0 daemon --config ${CONFIG_PATH}`,
     debug: true,
     launchTimeout: 240000,
     port: 7007,
