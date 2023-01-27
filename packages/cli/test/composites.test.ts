3c85f324a5f3936f045d60e8716be2a4aa739e8e
Paul Le Cam <paul@3box.io>
2023-01-27 14:11:20 +0000

    Fix lint error

--- packages/cli/test/composites.test.ts
+++ packages/cli/test/composites.test.ts
@@ -19,15 +19,16 @@ const MODEL2_JSON =
 describe('composites', () => {
   const seed = '3a6de55a5ef33d110a5a37438704b0f0cb77ca5977131775a70ffd1c23779c8c'
 
-  let undeployedComposite, encodedProfilesComposite
+  let undeployedComposite: Record<string, unknown>
+  let encodedProfilesComposite: Record<string, unknown>
   beforeAll(async () => {
     const [undeployed, profiles] = await Promise.all([
       fs.readJSON(
         fileURLToPath(new URL('mocks/encoded.composite.undeployed.json', import.meta.url))
-      ),
+      ) as Promise<Record<string, unknown>>,
       fs.readJSON(
-        fileURLToPath(new URL('./mocks/encoded.composite.profiles.json', import.meta.url))
-      ),
+        fileURLToPath(new URL('mocks/encoded.composite.profiles.json', import.meta.url))
+      ) as Promise<Record<string, unknown>>,
     ])
     undeployedComposite = undeployed
     encodedProfilesComposite = profiles
