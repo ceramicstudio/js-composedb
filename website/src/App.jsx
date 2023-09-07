236ba85b61efa6161648ee16ca62480e29120871
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-09-07 13:05:15 -0400

    Adding in button tracking

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,21 +1,41 @@
-import React from "react";
-import { GraphiQL } from "graphiql";
-import "graphiql/graphiql.min.css";
-import { ComposeClient } from "@composedb/client";
-import { definition } from "./definition.js";
+import React from 'react'
+import { GraphiQL } from 'graphiql'
+import 'graphiql/graphiql.min.css'
+import { ComposeClient } from '@composedb/client'
+import { definition } from './definition.js'
+import ReactGA from 'react-ga4'
 
 const composeClient = new ComposeClient({
-  ceramic: "https://ceramic-temp.hirenodes.io",
+  ceramic: 'https://ceramic-temp.hirenodes.io',
   definition,
-});
+})
 
+ReactGA.initialize('G-V2Y8T342EX')
 
 const fetcher = async (graphQLParams) => {
-  const data = await composeClient.executeQuery(`${graphQLParams.query}`);
+
+  ReactGA.event({
+    category: "sandbox-query",
+    action: "general-submit"
+  });
+
+  const data = await composeClient.executeQuery(`${graphQLParams.query}`)
   console.log(data)
-  return data.data;
-};
+  
+  if (data.errors) {
+    ReactGA.event({
+    category: "sandbox-query",
+    action: "query-error"
+  });
+  } else {
+    ReactGA.event({
+      category: "sandbox-query",
+      action: "query-success"
+    });
+  }
+  return data.data
+}
 
-const App = () => <GraphiQL fetcher={fetcher} />;
+const App = () => <GraphiQL fetcher={fetcher} />
 
-export default App;
+export default App
